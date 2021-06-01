11309be4b9c2663b2e285d4470e80133e4a6e1a5
Dale Jung <dale@dalejung.com>
2021-05-31 22:25:14 -0400

    ensure we don't start loading notebook until custom.js is run. This is perhaps less important now that we enact vim/gist.js via nbextensions.

--- notebook/static/notebook/js/main.js
+++ notebook/static/notebook/js/main.js
@@ -87,71 +87,66 @@ requirejs([
     // Pull typeahead from the global jquery object
     var typeahead = $.typeahead;
     
-    try{
-        requirejs(['custom/custom'], function() {});
-        bidi.loadLocale();
-    } catch(err) {
-        console.log("Error processing custom.js. Logging and continuing");
-        console.warn(err);
-    }
+    var setupNotebook = function() {
 
-    // compat with old IPython, remove for IPython > 3.0
-    window.CodeMirror = CodeMirror;
+      bidi.loadLocale();
+      // compat with old IPython, remove for IPython > 3.0
+      window.CodeMirror = CodeMirror;
 
-    // Setup all of the config related things
-    
+      // Setup all of the config related things
 
-    var common_options = {
+
+      var common_options = {
         ws_url : utils.get_body_data("wsUrl"),
         base_url : utils.get_body_data("baseUrl"),
         notebook_path : utils.get_body_data("notebookPath"),
         notebook_name : utils.get_body_data('notebookName')
-    };
+      };
 
-    var config_section = new configmod.ConfigSection('notebook', common_options);
-    config_section.load();
-    var common_config = new configmod.ConfigSection('common', common_options);
-    common_config.load();
+      var config_section = new configmod.ConfigSection('notebook', common_options);
+      config_section.load();
+      var common_config = new configmod.ConfigSection('common', common_options);
+      common_config.load();
 
-    // Instantiate the main objects
-    
-    var page = new pagemod.Page('div#header', 'div#site');
-    var pager = new pagermod.Pager('div#pager', {
+      // Instantiate the main objects
+
+      var page = new pagemod.Page('div#header', 'div#site');
+      var pager = new pagermod.Pager('div#pager', {
         events: events});
-    var acts = new actions.init();
-    var keyboard_manager = new keyboardmanager.KeyboardManager({
+      var acts = new actions.init();
+      var keyboard_manager = new keyboardmanager.KeyboardManager({
         pager: pager,
         events: events,
         actions: acts, 
         config: config_section,
-    });
-    var save_widget = new savewidget.SaveWidget('span#save_widget', {
+      });
+      var save_widget = new savewidget.SaveWidget('span#save_widget', {
         events: events,
         keyboard_manager: keyboard_manager});
-    acts.extend_env({save_widget:save_widget});
-    var contents = new contents_service.Contents({
-          base_url: common_options.base_url,
-          common_config: common_config
-        });
-    var notebook = new notebookmod.Notebook('div#notebook', $.extend({
+      acts.extend_env({save_widget:save_widget});
+      var contents = new contents_service.Contents({
+        base_url: common_options.base_url,
+        common_config: common_config
+      });
+      var notebook = new notebookmod.Notebook('div#notebook', $.extend({
         events: events,
         keyboard_manager: keyboard_manager,
         save_widget: save_widget,
         contents: contents,
         config: config_section},
         common_options));
-    var login_widget = new loginwidget.LoginWidget('span#login_widget', common_options);
-    var toolbar = new maintoolbar.MainToolBar('#maintoolbar-container', {
+      var login_widget = new loginwidget.LoginWidget('span#login_widget', common_options);
+      var toolbar = new maintoolbar.MainToolBar('#maintoolbar-container', {
         notebook: notebook,
         events: events,
         actions: acts});
-    var quick_help = new quickhelp.QuickHelp({
+      var quick_help = new quickhelp.QuickHelp({
         keyboard_manager: keyboard_manager,
         events: events,
         notebook: notebook});
-    keyboard_manager.set_notebook(notebook);
-    keyboard_manager.set_quickhelp(quick_help);
-    var menubar = new menubarmod.MenuBar('#menubar', $.extend({
+      keyboard_manager.set_notebook(notebook);
+      keyboard_manager.set_quickhelp(quick_help);
+      var menubar = new menubarmod.MenuBar('#menubar', $.extend({
         notebook: notebook,
         contents: contents,
         events: events,
@@ -160,91 +155,102 @@ requirejs([
         actions: acts,
         config: config_section},
         common_options));
-    var notification_area = new notificationarea.NotebookNotificationArea(
+      var notification_area = new notificationarea.NotebookNotificationArea(
         '#notification_area', {
-        events: events,
-        save_widget: save_widget,
-        notebook: notebook,
-        keyboard_manager: keyboard_manager});
-    notification_area.init_notification_widgets();
-    var kernel_selector = new kernelselector.KernelSelector(
+          events: events,
+          save_widget: save_widget,
+          notebook: notebook,
+          keyboard_manager: keyboard_manager});
+      notification_area.init_notification_widgets();
+      var kernel_selector = new kernelselector.KernelSelector(
         '#kernel_logo_widget', notebook);
-    searchandreplace.load(keyboard_manager);
+      searchandreplace.load(keyboard_manager);
 
-    $('body').append('<div id="fonttest"><pre><span id="test1">x</span>'+
-                     '<span id="test2" style="font-weight: bold;">x</span>'+
-                     '<span id="test3" style="font-style: italic;">x</span></pre></div>');
-    var nh = $('#test1').innerHeight();
-    var bh = $('#test2').innerHeight();
-    var ih = $('#test3').innerHeight();
-    if(nh != bh || nh != ih) {
+      $('body').append('<div id="fonttest"><pre><span id="test1">x</span>'+
+        '<span id="test2" style="font-weight: bold;">x</span>'+
+        '<span id="test3" style="font-style: italic;">x</span></pre></div>');
+      var nh = $('#test1').innerHeight();
+      var bh = $('#test2').innerHeight();
+      var ih = $('#test3').innerHeight();
+      if(nh != bh || nh != ih) {
         $('head').append('<style>.CodeMirror span { vertical-align: bottom; }</style>');
-    }
-    $('#fonttest').remove();
+      }
+      $('#fonttest').remove();
 
-    page.show();
+      page.show();
 
-    events.one('notebook_loaded.Notebook', function () {
+      events.one('notebook_loaded.Notebook', function () {
         var hash = document.location.hash;
         if (hash) {
-            document.location.hash = '';
-            document.location.hash = hash;
+          document.location.hash = '';
+          document.location.hash = hash;
         }
         notebook.set_autosave_interval(notebook.minimum_autosave_interval);
-    });
+      });
 
-    IPython.page = page;
-    IPython.notebook = notebook;
-    IPython.contents = contents;
-    IPython.pager = pager;
-    IPython.quick_help = quick_help;
-    IPython.login_widget = login_widget;
-    IPython.menubar = menubar;
-    IPython.toolbar = toolbar;
-    IPython.notification_area = notification_area;
-    IPython.keyboard_manager = keyboard_manager;
-    IPython.save_widget = save_widget;
-    IPython.tooltip = notebook.tooltip;
+      IPython.page = page;
+      IPython.notebook = notebook;
+      IPython.contents = contents;
+      IPython.pager = pager;
+      IPython.quick_help = quick_help;
+      IPython.login_widget = login_widget;
+      IPython.menubar = menubar;
+      IPython.toolbar = toolbar;
+      IPython.notification_area = notification_area;
+      IPython.keyboard_manager = keyboard_manager;
+      IPython.save_widget = save_widget;
+      IPython.tooltip = notebook.tooltip;
 
-    try {
+      try {
         events.trigger('app_initialized.NotebookApp');
-    } catch (e) {
+      } catch (e) {
         console.error("Error in app_initialized callback", e);
-    }
+      }
 
-    Object.defineProperty( IPython, 'actions', {
-      get: function() {
+      Object.defineProperty( IPython, 'actions', {
+        get: function() {
           console.warn('accessing "actions" on the global IPython/Jupyter is not recommended. Pass it to your objects constructors at creation time');
           return acts;
-      },
-      enumerable: true,
-      configurable: false
-    });
+        },
+        enumerable: true,
+        configurable: false
+      });
 
-    clipboard.setup_clipboard_events();
-    
-    // Now actually load nbextensionsload_extensions_from_config
-    Promise.all([
+      clipboard.setup_clipboard_events();
+
+      // Now actually load nbextensionsload_extensions_from_config
+      Promise.all([
         utils.load_extensions_from_config(config_section),
         utils.load_extensions_from_config(common_config),
-    ])
-    .catch(function(error) {
-        console.error('Could not load nbextensions from user config files', error);
-    })
-    // BEGIN HARDCODED WIDGETS HACK
-    .then(function() {
-        if (!utils.is_loaded('jupyter-js-widgets/extension')) {
+      ])
+        .catch(function(error) {
+          console.error('Could not load nbextensions from user config files', error);
+        })
+      // BEGIN HARDCODED WIDGETS HACK
+        .then(function() {
+          if (!utils.is_loaded('jupyter-js-widgets/extension')) {
             // Fallback to the ipywidgets extension
             utils.load_extension('widgets/notebook/js/extension').catch(function () {
-                console.warn('Widgets are not available.  Please install widgetsnbextension or ipywidgets 4.0');
+              console.warn('Widgets are not available.  Please install widgetsnbextension or ipywidgets 4.0');
             });
-        }
-    })
-    .catch(function(error) {
-        console.error('Could not load ipywidgets', error);
-    });
-    // END HARDCODED WIDGETS HACK
+          }
+        })
+        .catch(function(error) {
+          console.error('Could not load ipywidgets', error);
+        });
+      // END HARDCODED WIDGETS HACK
 
-    notebook.load_notebook(common_options.notebook_path);
+      notebook.load_notebook(common_options.notebook_path);
+    }
+
+    try{
+      requirejs(['custom/custom'], function() {
+        setupNotebook();
+      });
+    } catch(err) {
+        setupNotebook();
+        console.log("Error processing custom.js. Logging and continuing");
+        console.warn(err);
+    }
 
 });
